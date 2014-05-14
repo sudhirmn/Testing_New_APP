fabbaab4274869bce7372814b9bec0f3171b7e37
sudhirmn <you@example.com>
2014-05-14 01:18:47 -0700

    properties file

---
+++ src/com/test/sudhir/test/HelloTest.java
@@ -0,0 +1,10 @@
+package com.test.sudhir.test;
+
+public class HelloTest {
+	public static void main(String args[]){
+		
+		System.out.println("Hello Test 1");
+		System.out.println("Hello Test 2");
+		System.out.println("Hello Test 3");
+	}
+}
